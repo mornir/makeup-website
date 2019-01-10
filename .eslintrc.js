e33b02a0c59421fe793dd7491b93965dda33d644
mornir <jeromepott@hotmail.com>
2019-01-10 19:38:43 +0100

    Turn off some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,23 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
   ],
+
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-indent': 'off',
+    'no-console': 'off',
+  },
 }
