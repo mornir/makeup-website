c91852885a8c007eb431ff1ef83d964a56445aaa
mornir <jeromepott@hotmail.com>
2019-01-13 11:19:40 +0100

    Add alternative color

--- tailwind.js
+++ tailwind.js
@@ -124,6 +124,7 @@ let colors = {
   'pink-darker': '#870557', // USED for logo
   'pink-dark': '#eb5286',
   'pink': '#E8368F', // USED
+  'pink-alt': '#F9007D',
   'pink-light': '#F364A2', // USED
   'pink-lighter': '#FFB8D2',
   'pink-lightest': '#FFE3EC'
