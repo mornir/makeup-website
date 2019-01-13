c6f41f9ded796c5d638e7fa832ee0abaa67fcadd
mornir <jeromepott@hotmail.com>
2019-01-13 14:28:53 +0100

    Start work on about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>About Page</h1>
+  <section>
+    <h2>About me</h2>
+    <p>Professional makeup artist, working with techniques, creativity, patience and great passion, I've thrown myself into the world of cosmetics for more than ten years, and the belief of beauty makes the change has only been stronger. I studied in Makeup school Studio B in Lausanne and acquired my diploma as a professional makeup artist. Work for all occasions.</p>
+  </section>
 </template>
 
 <script>
