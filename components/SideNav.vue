54ec6e11b70e33c9b7399640febe2f7b325af01c
mornir <jeromepott@hotmail.com>
2019-01-13 13:35:24 +0100

    Add more specific name

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -38,14 +38,14 @@ export default {
 aside {
   -webkit-clip-path: circle(0px at 98% 5px);
   clip-path: circle(0px at 98% 5px);
-  transition: all 0.6s cubic-bezier(0.895, 0.03, 0.685, 0.22);
+  transition: clip-path 0.6s cubic-bezier(0.895, 0.03, 0.685, 0.22);
   pointer-events: none;
 }
 
 aside.open {
   -webkit-clip-path: circle(100vw at 98% 50px);
   clip-path: circle(100vw at 98% 50px);
-  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  transition: clip-path 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
   pointer-events: all;
 }
 
