df1bc611384f0f339d7bcdce827f30895ef78a7d
mornir <jeromepott@hotmail.com>
2019-01-22 21:36:00 +0100

    Add courses

--- pages/makeups.vue
+++ pages/makeups.vue
@@ -1,13 +1,23 @@
 <template>
-  <section>
+  <div class="pb-8">
+    <section>
+      <h1 class="text-center text-pink-lightest my-8">Makeups</h1>
+      <div class="makeups-grid">
+        <AppPackage v-for="makeup in makeups"
+                    :key="makeup.title"
+                    :makeup="makeup" />
+      </div>
+    </section>
 
-    <h1 class="text-center text-pink-lightest my-8">Makeups</h1>
-    <div class="makeups-grid">
-      <AppPackage v-for="makeup in makeups"
-                  :key="makeup.title"
-                  :makeup="makeup" />
-    </div>
-  </section>
+    <section>
+      <h1 class="text-center text-pink-lightest my-8">Courses</h1>
+      <div class="makeups-grid">
+        <AppPackage v-for="course in courses"
+                    :key="course.title"
+                    :makeup="course" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -18,18 +28,34 @@ export default {
       makeups: [
         {
           title: 'Bridal Makeup',
+          slug: 'bridal-makeup',
           desc: 'Look at your best on this unique day.',
           features: ['Test included', 'Only 50 CHF per additional quest'],
           price: 190,
         },
         {
           title: 'Occasional Makeup',
+          slug: 'occasional-makeup',
           desc: 'Have all eyes gaze on you.',
           features: [
             'For all sorts of occasions!',
             'Only 40 CHF per additional guest',
           ],
           price: 90,
+        },
+      ],
+      courses: [
+        {
+          title: 'Private Course',
+          slug: 'private-course',
+          desc: 'lorem ipsum',
+          features: ['Choose your own theme (base makeup, eye makeup, etc.)'],
+        },
+        {
+          title: 'Comprehensive Course',
+          slug: 'comprehensive-makeup-course',
+          desc: 'lorem ipsum',
+          features: ['Choose your own theme (base makeup, eye makeup, etc.)'],
         },
       ],
     }
