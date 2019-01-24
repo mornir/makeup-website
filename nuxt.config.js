e867c61d86d01d6b334e9562e4b4dce20a524f9b
mornir <jeromepott@hotmail.com>
2019-01-24 18:22:29 +0100

    Add favicon Close #13

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
   },
 
   /*
