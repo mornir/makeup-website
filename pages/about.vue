b5466c6d996ea1a7560116880d83a95a8378546a
mornir <jeromepott@hotmail.com>
2019-01-26 18:38:33 +0100

    Translate page

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,16 @@
+<i18n>
+{
+  "en": {
+    "title": "About me",
+    "text": "Professional makeup artist, working with techniques, creativity, patience and great passion, I've thrown myself into the world of cosmetics for more than ten years, and the belief of beauty makes the change has only been stronger. I studied in Makeup school Studio B in Lausanne and acquired my diploma as a professional makeup artist. Work for all occasions."
+  },
+  "fr": {
+    "title": "À mon propos",
+    "text": "Maquilleuse professionnelle, travaillant avec technique, créativité, patience et grande passion, je me suis lancée dans le monde du maquillage il y a plus de dix ans, et la quête de la beauté ne fait que renforcer ma détermination à perfectionner à mon art. J'ai étudié le maquillage à l'école Studio B à Lausanne, où j'ai obtenu mon diplôme de maquilleuse professionnelle. J'offre mes services de maquilleuses pour toute occasion."
+  }
+}
+</i18n>
+
 <template>
 
   <section class="height-minus-nav text-pink-lightest mx-8 md:flex items-end">
@@ -7,9 +20,9 @@
          alt="about">
 
     <div class="pt-5 self-center max-w-md">
-      <h2 class="mb-4 pl-3 border-l-8 border-pink-alt">About me</h2>
+      <h2 class="mb-4 pl-3 border-l-8 border-pink-alt">{{ $t('title') }}</h2>
       <p class="text-lg font-medium leading-tight">
-        Professional makeup artist, working with techniques, creativity, patience and great passion, I've thrown myself into the world of cosmetics for more than ten years, and the belief of beauty makes the change has only been stronger. I studied in Makeup school Studio B in Lausanne and acquired my diploma as a professional makeup artist. Work for all occasions.</p>
+        {{ $t('text') }}</p>
     </div>
 
     <img src="@/assets/img/about-desktop.png"
