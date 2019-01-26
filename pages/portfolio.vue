792e2f80b2e54de5e36c7792b11904bcfaa58413
mornir <jeromepott@hotmail.com>
2019-01-26 18:42:54 +0100

    Translate portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,18 @@
+<i18n>
+{
+  "en": {
+    "title": "Coming soon."
+  },
+  "fr": {
+    "title": "En construction."
+  }
+}
+</i18n>
+
 <template>
-  <h1 class="italic text-pink-lightest">Coming Soon~~~</h1>
+  <div class="height-minus-nav flex justify-center items-center">
+    <h1 class="italic text-pink-lightest">{{ $t('title') }}</h1>
+  </div>
 </template>
 
 <script>
@@ -7,4 +20,7 @@ export default {}
 </script>
 
 <style>
+.height-minus-nav {
+  min-height: calc(100vh - var(--nav-height));
+}
 </style>
