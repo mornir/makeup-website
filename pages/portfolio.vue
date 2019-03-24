7a6d0229904f71b3658e7b8c1ba230d1962af38c
mornir <jeromepott@hotmail.com>
2019-03-24 11:57:18 +0100

    Scroll function

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -10,21 +10,50 @@
 </i18n>
 
 <template>
-  <div class="w-screen bg-pink-lightest flex overflow-x-auto h-screen scroll-container ">
+  <div class="w-screen bg-pink-lightest flex overflow-x-auto h-screen scroll-container "
+       ref="container">
+
+    <button class="absolute pin-l  p-8"
+            id="previous"
+            @click="scrollLeft">
+      <i class="fas fa-arrow-left text-white fa-2x" />
+    </button>
+
     <img src="@/assets/photos/photo-1.jpg"
          alt="photo">
     <img src="@/assets/photos/photo-2.jpg"
          alt="photo">
     <img src="@/assets/photos/photo-3.jpg"
          alt="photo">
+
+    <button class="absolute pin-r p-8"
+            id="next"
+            @click="scrollRight">
+      <i class="fas fa-arrow-right text-white fa-2x" />
+    </button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    scrollRight() {
+      this.$refs.container.scrollBy({
+        left: window.innerWidth,
+        behavior: 'smooth',
+      })
+    },
+    scrollLeft() {
+      this.$refs.container.scrollBy({
+        left: -window.innerWidth,
+        behavior: 'smooth',
+      })
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
 .height-minus-nav {
   min-height: calc(100vh - var(--nav-height));
 }
@@ -32,9 +61,23 @@ export default {}
 img {
   flex: 0 0 auto;
   scroll-snap-align: center;
+  object-fit: cover;
+  object-position: center center;
 }
 
 .scroll-container {
   scroll-snap-type: x mandatory;
+}
+
+#next {
+  top: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+}
+
+#previous {
+  top: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
 }
 </style>
