03450b57c1c5e63fc788c860775bc60eabd8fede
mornir <jeromepott@hotmail.com>
2019-05-17 20:53:40 +0200

    Add baseURL to nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,6 @@ export default {
   /*
    ** Headers of the page
    */
-
-  server: {
-    port: 3000, // default: 3000
-  },
-
   head: {
     title: 'Soline Wang',
     meta: [
@@ -103,6 +98,13 @@ export default {
       },
     ],
   ],
+
+  /*
+   ** Axios config
+   */
+  axios: {
+    baseURL: '/',
+  },
 
   vue: {
     config: {
