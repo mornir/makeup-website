a565746e9d29544334a74503506e1f1fd713b3e9
mornir <jeromepott@hotmail.com>
2019-05-17 12:17:46 +0200

    Fix form modal

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,45 +1,52 @@
 
 <template>
   <div>
-    <div class="text-pink-lightest bg-pink-darkest height-minus-nav">
+    <div class="text-pink-lightest bg-pink-darkest height-contact-page">
       <div class="max-w-sm mx-auto">
         <section class="py-8 mx-4">
           <h2 class="mb-4 pl-3 border-l-8 border-pink-alt text-3xl">Contact</h2>
           <p class="text-xl">Don’t hesitate to contact me. You can also find my creations on instragam, on my portfolio and on youtube.</p>
         </section>
         <section class="flex justify-around">
-          <button @click="show('qr-code')">
-            <Weixin class="h-10 text-pink-lighter fill-current hover:text-pink-alt" />
+          <button @click="showEmail = false; showQR = true"
+                  class="focus:outline-none">
+            <Weixin class="h-10 fill-current hover:text-pink-alt"
+                    :class="showQR ? 'text-pink-alt' : 'text-pink-lighter'" />
           </button>
           <a href="https://www.instagram.com/solinewangmua/"
              target="_blank"
-             rel="noopener">
+             rel="noopener"
+             class="focus:outline-none">
             <Instagram class="h-10 text-pink-lighter fill-current hover:text-pink-alt" />
           </a>
           <a href="https://www.youtube.com/channel/UCVeQ_Od2gp9OVRowSFJKPJw"
              target="_blank"
-             rel="noopener">
+             rel="noopener"
+             class="focus:outline-none">
             <YouTube class="h-10 text-pink-lighter fill-current hover:text-pink-alt" />
           </a>
 
-          <button @click="show('email')">
-            <EmailIcon class="h-10 text-pink-lighter fill-current hover:text-pink-alt" />
+          <button @click="showEmail = true; showQR = false"
+                  class="focus:outline-none">
+            <EmailIcon class="h-10 fill-current hover:text-pink-alt "
+                       :class="showEmail ? 'text-pink-alt' : 'text-pink-lighter'" />
           </button>
 
+        </section>
+
+        <section class="mt-8 pb-8 text-center">
+          <EmailForm v-if="showEmail" />
+
+          <img src="@/assets/img/weixin_qr_code.jpg"
+               class="h-64"
+               alt="Weixin QR Code"
+               v-if="showQR">
         </section>
 
       </div>
 
     </div>
-    <EmailModal />
 
-    <modal name="qr-code"
-           height="auto"
-           width="300">
-
-      <img src="@/assets/img/weixin_qr_code.jpg"
-           alt="Weixin QR Code">
-    </modal>
   </div>
 </template>
 
@@ -49,7 +56,7 @@ import Weixin from '@/assets/svg/weixin-brands.svg'
 import Instagram from '@/assets/svg/instagram-brands.svg'
 import YouTube from '@/assets/svg/youtube-brands.svg'
 
-import EmailModal from '@/components/EmailModal'
+import EmailForm from '@/components/EmailForm'
 
 export default {
   components: {
@@ -57,21 +64,25 @@ export default {
     Instagram,
     EmailIcon,
     YouTube,
-    EmailModal,
+    EmailForm,
   },
-  methods: {
-    show(modalName) {
-      this.$modal.show(modalName)
-    },
-    hide() {
-      this.$modal.hide(modalName)
-    },
-  },
+
   data() {
-    return {}
+    return {
+      showEmail: false,
+      showQR: false,
+    }
   },
 }
 </script>
 
-<style scoped>
+<style scoped lang="postcss">
+.height-contact-page {
+  min-height: calc(100vh - var(--nav-height));
+}
+
+button:focus svg,
+a:focus svg {
+  @apply text-pink-alt;
+}
 </style>
