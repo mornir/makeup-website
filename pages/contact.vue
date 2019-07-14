a688163301b8a22e93631c0bba13386e93a9b8ff
mornir <jeromepott@hotmail.com>
2019-07-14 13:33:53 +0200

    Update

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,66 +1,61 @@
 
 <template>
-  <div class="text-pink-lightest bg-pink-200 height-contact-page">
+  <div class="text-pink-lightest bg-pink-200 h-full">
     <div class="max-w-sm mx-auto">
 
+      <section class="py-8 mx-4">
 
-        <section class="py-8 mx-4">
+        <h1 class="mb-4 pl-3 border-l-8 border-pink-alt text-3xl">Follow me</h1>
+        <p class="text-xl">I regularly share my work on Instagram and I have a YouTube channel where I make makeup tutorial videos (in Chinese).</p>
+        <div class="flex justify-evenly mt-4">
 
-          <h1 class="mb-4 pl-3 border-l-8 border-pink-alt text-3xl">Follow me</h1>
-          <p class="text-xl">I regularly share my work on Instagram and I have a YouTube channel where I make makeup tutorial videos (in Chinese).</p>
-          <div class="flex justify-evenly mt-4">
+          <a href="https://www.instagram.com/solinewangmua/"
+             target="_blank"
+             rel="noopener"
+             class="focus:outline-none">
+            <Instagram class="h-10 text-pink-900 fill-current hover:text-pink-alt" />
+          </a>
+          <a href="https://www.youtube.com/channel/UCVeQ_Od2gp9OVRowSFJKPJw"
+             target="_blank"
+             rel="noopener"
+             class="focus:outline-none">
+            <YouTube class="h-10 text-pink-900 fill-current hover:text-pink-alt" />
+          </a>
+        </div>
 
-            <a href="https://www.instagram.com/solinewangmua/"
-               target="_blank"
-               rel="noopener"
-               class="focus:outline-none">
-              <Instagram class="h-10 text-pink-900 fill-current hover:text-pink-alt" />
-            </a>
-            <a href="https://www.youtube.com/channel/UCVeQ_Od2gp9OVRowSFJKPJw"
-               target="_blank"
-               rel="noopener"
-               class="focus:outline-none">
-              <YouTube class="h-10 text-pink-900 fill-current hover:text-pink-alt" />
-            </a>
-          </div>
+      </section>
+      <section class="py-8 mx-4">
+        <h1 class="mb-4 pl-3 border-l-8 border-pink-alt text-3xl">Contact me</h1>
+        <p class="text-xl">Don’t hesitate to contact me. You can text me on Wechat and Whatsapp, or send me an email.</p>
+        <div class="flex justify-evenly mt-4">
 
-        </section>
-        <section class="py-8 mx-4">
-          <h1 class="mb-4 pl-3 border-l-8 border-pink-alt text-3xl">Contact me</h1>
-          <p class="text-xl">Don’t hesitate to contact me. You can text me on Wechat and Whatsapp, or send me an email.</p>
-          <div class="flex justify-evenly mt-4">
-           
-            <a href="https://www.wechat.com/ECtmVhXr_IQ-OaG8r6GKGN8"
-            target="_blank" rel="noopener nofollow"
-                    class="focus:outline-none">
-              <Weixin class="h-10 fill-current hover:text-pink-alt text-pink-900"
-                  />
-            </a>
-            <a href="https://wa.me/410787498068"
-            target="_blank" rel="noopener nofollow"
-                    class="focus:outline-none">
-              <Whatsapp class="h-10 fill-current hover:text-pink-alt text-pink-900"
-                    />
-            </a>
-            <button @click="showEmail"
-                    class="focus:outline-none">
-              <EmailIcon class="h-10 fill-current hover:text-pink-alt"
-                         :class="isEmailVisible ? 'text-pink-alt' : 'text-pink-900'" />
-            </button>
-          </div>
+          <a href="https://www.wechat.com/ECtmVhXr_IQ-OaG8r6GKGN8"
+             target="_blank"
+             rel="noopener nofollow"
+             class="focus:outline-none">
+            <Weixin class="h-10 fill-current hover:text-pink-alt text-pink-900" />
+          </a>
+          <a href="https://wa.me/410787498068"
+             target="_blank"
+             rel="noopener nofollow"
+             class="focus:outline-none">
+            <Whatsapp class="h-10 fill-current hover:text-pink-alt text-pink-900" />
+          </a>
+          <button @click="showEmail"
+                  class="focus:outline-none">
+            <EmailIcon class="h-10 fill-current hover:text-pink-alt"
+                       :class="isEmailVisible ? 'text-pink-alt' : 'text-pink-900'" />
+          </button>
+        </div>
 
-          <div class="mt-8 pb-8 ">
+        <div class="mt-8 pb-8 ">
           <EmailForm v-show="isEmailVisible"
                      ref="form" />
 
-     
+        </div>
+      </section>
 
-          </div>
-        </section>
- 
-
-      </div>
-
+    </div>
 
   </div>
 </template>
@@ -102,10 +97,6 @@ export default {
 </script>
 
 <style scoped lang="postcss">
-.height-contact-page {
-  min-height: calc(100vh - var(--nav-height));
-}
-
 button:focus svg,
 a:focus svg {
   @apply text-pink-alt;
