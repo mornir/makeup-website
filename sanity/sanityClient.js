fa86755b37c0993c9a9dbe97d215e87d03fc690a
mornir <jeromepott@hotmail.com>
2020-11-29 11:24:27 +0100

    Update use picosanity

--- sanity/sanityClient.js
+++ sanity/sanityClient.js
@@ -1,10 +1,10 @@
-import sanityClient from '@sanity/client'
+import sanityClient from 'picosanity'
 
 export default sanityClient({
   // Find your project ID and dataset in `sanity.json` in your studio project
   projectId: '268fhabe',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
